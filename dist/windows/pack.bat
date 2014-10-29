9393be48fd8a53a9c653ccedfb3be8d3dace1ce2
Hannes Matuschek <hmatuschek@gmail.com>
2014-10-29 14:08:50 +0100

    Fixed windows build-script.

--- dist/windows/pack.bat
+++ dist/windows/pack.bat
@@ -1,5 +1,6 @@
 SET PATH=C:/Program Files (x86)/NSIS;%PAHT%
 SET QT_PATH=C:/Qt/Qt5.3.2/5.3/mingw482_32/bin
+SET QT_PLUGIN_PATH=C:/Qt/Qt5.3.2/5.3/mingw482_32/plugins
 SET LIB_PATH=C:/MinGW/bin
 
 COPY "%QT_PATH%/Qt5Core.dll" .
@@ -13,5 +14,7 @@ COPY "%QT_PATH%/libwinpthread-1.dll" .
 COPY "%LIB_PATH%/libportaudio-2.dll" .
 COPY "%LIB_PATH%/libgcc_s_dw2-1.dll" .
 COPY "%LIB_PATH%/libstdc++-6.dll" .
+COPY "%QT_PLUGIN_PATH/platform/qwindows.dll" .
+COPY "%QT_PLUGIN_PATH/imageformats/qsvg.dll" .
 
 makensis kochmorse.nsi
